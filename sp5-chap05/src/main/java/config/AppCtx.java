d516dcae6c3d649b5b11d5acd84c368890bcacb7
cdnwell <cdnwellhk@gmail.com>
2022-12-06 20:12:16 +0900

    commit spring5 12/06

--- sp5-chap05/src/main/java/config/AppCtx.java
+++ sp5-chap05/src/main/java/config/AppCtx.java
@@ -2,34 +2,19 @@ package config;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
-import spring.ChangePasswordService;
 import spring.MemberDao;
-import spring.MemberInfoPrinter;
-import spring.MemberListPrinter;
 import spring.MemberPrinter;
-import spring.MemberRegisterService;
 import spring.MemberSummaryPrinter;
 import spring.VersionPrinter;
 
 @Configuration
+@ComponentScan(basePackages = {"spring"})
 public class AppCtx {
-
-	@Bean
-	public MemberDao memberDao() {
-		return new MemberDao();
-	}
-
-	@Bean
-	public MemberRegisterService memberRegSvc() {
-		return new MemberRegisterService(memberDao());
-	}
-
-	@Bean
-	public ChangePasswordService changePwdSvc() {
-		return new ChangePasswordService();
-	}
 
 	@Bean
 	@Qualifier("printer")
@@ -41,18 +26,6 @@ public class AppCtx {
 	@Qualifier("summaryPrinter")
 	public MemberSummaryPrinter memberPrinter2() {
 		return new MemberSummaryPrinter();
-	}
-
-	@Bean
-	public MemberListPrinter listPrinter() {
-		return new MemberListPrinter();
-	}
-
-	@Bean
-	public MemberInfoPrinter infoPrinter() {
-		MemberInfoPrinter infoPrinter = new MemberInfoPrinter();
-		infoPrinter.setPrinter(memberPrinter2());
-		return infoPrinter;
 	}
 
 	@Bean
