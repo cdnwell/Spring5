98b6c39993295d286d92c861279e625fb6f200b2
cdnwell <cdnwellhk@gmail.com>
2022-12-15 19:17:24 +0900

    commit spring5 12-15

--- sp5-chap08/src/main/java/spring/MemberDao.java
+++ sp5-chap08/src/main/java/spring/MemberDao.java
@@ -1,23 +1,70 @@
 package spring;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 public class MemberDao {
 
-	public Member selectByEmail(String email) {
-		return null;
-	}
-
-	public void insert(Member member) {
-	}
-
-	public void update(Member member) {
+	private JdbcTemplate jdbcTemplate;
+	
+	public MemberDao(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 	
-	public Collection<Member> selectAll(){
-		return null;
+	public Member selectByEmail(String email) {
+		List<Member> results = jdbcTemplate.query(
+				"select * from MEMBER where EMAIL = ?",
+				new RowMapper<Member>() {
+					@Override
+					public Member mapRow(ResultSet rs, int rowNum) throws SQLException{
+						Member member = new Member(
+								rs.getString("EMAIL"),
+								rs.getString("PASSWORD"),
+								rs.getString("NAME"),
+								rs.getTimestamp("REGDATE").toLocalDateTime()
+								);
+						member.setId(rs.getLong("ID"));
+						return member;
+					}
+				},
+				email);
+			
+		return results.isEmpty() ? null : results.get(0);
 	}
-
+	
+	public List<Member> selectAll(){
+		List<Member> results = jdbcTemplate.query("select * from MEMBER", 
+				new RowMapper<Member>() {
+			@Override
+			public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Member member = new Member(
+						rs.getString("EMAIL"),
+						rs.getString("PASSWORD"),
+						rs.getString("NAME"),
+						rs.getTimestamp("REGDATE").toLocalDateTime()
+						);
+				member.setId(rs.getLong("ID"));
+				return member;
+			}
+		});
+		return results;
+	}
+	
+	public int count() {
+		Integer count = jdbcTemplate.queryForObject("select count(*) from MEMBER", Integer.class);
+		return count;
+	}
+	
+	public void update(Member member) {
+		jdbcTemplate.update(
+				"update MEMBER set NAME = ?, PASSWORD = ?, where EMAIL = ?",
+				member.getName(), member.getPassword(), member.getEmail()
+				);
+	}
+	
 }
