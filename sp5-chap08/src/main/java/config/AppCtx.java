8e7bae88d92989e31fc2c37b482f7a93660a9deb
cdnwell <cdnwellhk@gmail.com>
2022-12-17 10:36:02 +0900

    commit spring5

--- sp5-chap08/src/main/java/config/AppCtx.java
+++ sp5-chap08/src/main/java/config/AppCtx.java
@@ -4,7 +4,10 @@ package config;
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
+import spring.ChangePasswordService;
 import spring.MemberDao;
 
 @Configuration
@@ -13,11 +16,12 @@ public class AppCtx {
 	@Bean(destroyMethod="close")
 	public DataSource dataSource() {
 		DataSource ds = new DataSource();
-		ds.setDriverClassName("com.mysql.jdbc.Driver");
+		ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
 		ds.setUrl("jdbc:mysql://localhost/spring5fs?characterEncoding=utf8");
 		ds.setUsername("spring5");
 		ds.setPassword("spring5");
 		ds.setInitialSize(2);
+		ds.setMaxIdle(10);
 		ds.setMaxActive(10);
 		ds.setTestWhileIdle(true);	// 유휴 커넥션 검사
 		ds.setMinEvictableIdleTimeMillis(1000*60*3);	// 최소 유휴 시간 3분
@@ -26,8 +30,23 @@ public class AppCtx {
 	}
 	
 	@Bean
+	public PlatformTransactionManager transactionManager() {
+		DataSourceTransactionManager tm = new DataSourceTransactionManager();
+		tm.setDataSource(dataSource());
+		return tm;
+	}
+			
+	
+	@Bean
 	public MemberDao memberDao() {
 		return new MemberDao(dataSource());
+	}
+	
+	@Bean
+	public ChangePasswordService changePwdSvc() {
+		ChangePasswordService pwdSvc = new ChangePasswordService();
+		pwdSvc.setMemberDao(memberDao());
+		return pwdSvc;
 	}
 
 }
