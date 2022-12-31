4c33edebd73d0e0fafc7552eca124250238a0c19
cdnwell <cdnwellhk@gmail.com>
2022-12-31 12:55:02 +0900

    commit spring 5

--- sp5-chap16/src/main/java/controller/RestMemberController.java
+++ sp5-chap16/src/main/java/controller/RestMemberController.java
@@ -4,14 +4,21 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import spring.DuplicateMemberException;
 import spring.Member;
 import spring.MemberDao;
 import spring.MemberRegisterService;
+import spring.RegisterRequest;
 
 @RestController
 public class RestMemberController {
@@ -24,13 +31,13 @@ public class RestMemberController {
 	}
 
 	@GetMapping("/api/members/{id}")
-	public Member member(@PathVariable("id") Long id, HttpServletResponse response) throws IOException {
+	public ResponseEntity<Object> member(@PathVariable("id") Long id, HttpServletResponse response) throws IOException {
 		Member member = memberDao.selectById(id);
 		if (member == null) {
-			response.sendError(HttpServletResponse.SC_NOT_FOUND);
-			return null;
+			return ResponseEntity.status(HttpStatus.NOT_FOUND)
+					.body(new ErrorResponse("no member"));
 		}
-		return member;
+		return ResponseEntity.status(HttpStatus.OK).body(member);
 	}
 
 	public void setMemberDao(MemberDao memberDao) {
@@ -39,6 +46,19 @@ public class RestMemberController {
 
 	public void setRegisterService(MemberRegisterService registerService) {
 		this.registerService = registerService;
+	}
+	
+	@PostMapping("/api/members")
+	public void newMember(
+			@RequestBody @Valid RegisterRequest regReq,
+			HttpServletResponse response) throws IOException {
+		try {
+			Long newMemberId = registerService.regist(regReq);
+			response.setHeader("Location", "/api/members/"+newMemberId);
+			response.setStatus(HttpServletResponse.SC_CREATED);
+		} catch (DuplicateMemberException dupEx) {
+			response.sendError(HttpServletResponse.SC_CONFLICT);
+		}
 	}
 
 }
